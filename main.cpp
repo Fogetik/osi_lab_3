b806fbaa325eaaf6649b135788221e1f934b5be3
Fogetik <danila041202@gmail.com>
2022-06-11 19:54:43 +0300

    some fix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unistd.h>
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <cmath>
@@ -38,10 +40,38 @@ void task_4();
 
 void dLatch(bool choice);
 
+void dLatch1(bool choice);
+
 int main()
 {
-//    cout << alu8(1, 1, "00100111", "01100001", 1);
-
+//    firstTask();
+//    string line1, line2;
+//    cout << "First expression: ";
+//    std::getline(cin, line1);
+//    std::transform(line1.begin(), line1.end(), line1.begin(), tolower);
+//    line1 = del_space(line1);
+//
+//    vector<int> result_1 = tab_truth(line1);
+//    for (int d : result_1){
+//        cout << d << endl;
+//    }
+//
+//    cout << "Second expression: ";
+//    std::getline(cin, line2);
+//    std::transform(line2.begin(), line2.end(), line2.begin(), tolower);
+//
+//    line2 = del_space(line2);
+//
+//    vector<int> result_2 = tab_truth(line2);
+//    for (int d : result_2){
+//        cout << d << endl;
+//    }
+//
+//    if (result_1 == result_2)
+//        cout << "Equality" << endl;
+//    else
+//        cout << "Not equality" << endl;
+//    (!(A+(B*C)+(A+C)))*(!B*!C*(A+!B))
     task_4();
     return 0;
 }
@@ -189,6 +219,7 @@ string to_binary(int count, ull d_size)
 
 vector <int> tab_truth(string& line)
 {
+    bool cos = false;
     vector<char> dictionary;
     dictionary = take_all_var(line);
     line = postfix(line);
@@ -196,12 +227,13 @@ vector <int> tab_truth(string& line)
     vector<int> table;
     map<char, bool> dic_v2;
     string bin;
-    bool var_1, var_2, var_3, res_1, res_2, p_res_1, p_res_2, p_var_1, p_var_2, p_var_3;
+    bool var_1, var_2, var_3, p_var_1, p_var_2, p_var_3;
+    bool var_4, p_var_4;
     int count = static_cast<int>(dictionary.size());
     for (int i = 0; i < pow(2,count); i++)
     {
-        var_1 = false; var_2 = false; var_3 = false; res_1 = false; res_2 = false;
-        p_res_1 = false; p_res_2 = false; p_var_1 = false; p_var_2 = false; p_var_3 = false;
+        var_1 = false; var_2 = false; var_3 = false; var_4 = false;
+        p_var_1 = false; p_var_2 = false; p_var_3 = false; p_var_4 = false;
 
         bin = to_binary(i, dictionary.size());
 
@@ -214,6 +246,7 @@ vector <int> tab_truth(string& line)
         {
             if ((sym != '*') && (sym != '+') && (sym != '!'))
             {
+                cos = false;
                 if (!p_var_1)
                 {
                     if ((sym == '0') || (sym == '1'))
@@ -225,92 +258,99 @@ vector <int> tab_truth(string& line)
                 }
                 else if (!p_var_2)
                 {
+                    cos = true;
                     if ((sym == '0') || (sym == '1'))
                         var_2 = static_cast<bool>(sym - '0');
                     else
                         var_2 = dic_v2[sym];
 
                     p_var_2 = true;
+
                 }
                 else if (!p_var_3)
                 {
-                    var_3 = var_1;
+//                    var_3 = var_1;
                     if ((sym == '0') || (sym == '1'))
-                        var_1 = static_cast<bool>(sym - '0');
+                        var_3 = static_cast<bool>(sym - '0');
                     else
-                        var_1 = dic_v2[sym];
+                        var_3 = dic_v2[sym];
 
                     p_var_3 = true;
                 }
+                else if (!p_var_4)
+                {
+//                    var_3 = var_1;
+                    if ((sym == '0') || (sym == '1'))
+                        var_4 = static_cast<bool>(sym - '0');
+                    else
+                        var_4 = dic_v2[sym];
+
+                    p_var_4 = true;
+                }
+
+
             }
             else
             {
                 if (sym == '!')
                 {
-                    if ((p_var_1)&&(!p_var_2))
-                        var_1 = !var_1;
-                    else if ((!p_res_1) && (!p_res_2))
+
+                    if (p_var_4){
+                        var_4 = !var_4;
+                    }
+                    else if (p_var_3){
+                        var_3 = !var_3;
+                    }
+                    else if (p_var_2){
                         var_2 = !var_2;
-                    else if ((p_res_1)&&(!p_res_2))
-                        res_1 = !res_1;
-                    else
-                        res_2 = !res_2;
+                    }
+                    else if (p_var_1){
+                        var_1 = !var_1;
+                    }
+
+
                 }
                 else if (sym == '*')
                 {
-                    if (((!p_res_1) || (!p_var_2)) && (!p_res_2)) {
-                        if (!p_var_2)
-                        {
-                            if (!p_var_3)
-                                res_1 = res_1 && var_1;
-                            else
-                                res_1 = res_1 && var_3;
 
-                        }
-                        else
-                            res_1 = var_1 && var_2;
-
-                        p_res_1 = true;
+                    if (p_var_3 && p_var_4){
+                        p_var_4 = false;
+                        var_3 = var_3 && var_4;
                     }
-                    else if (!p_res_2) {
-                        res_2 = var_1 && var_2;
-                        p_res_2 = true;
+                    else if (p_var_2 && p_var_3){
+                        p_var_3 = false;
+                        var_2 = var_2 && var_3;
                     }
-                    else
-                    {
-                        res_1 = res_1 && res_2;
-                        p_res_2 = false;
+                    else if (p_var_1 && p_var_2){
+                        p_var_2 = false;
+                        var_1 = var_1 && var_2;
                     }
 
-                    p_var_1 = false; p_var_2 = false;
+
+
+
+
+
                 }
                 else
                 {
-                    if (((!p_res_1) || (!p_var_2))&& (!p_res_2)) {
-                        if (!p_var_2)
-                        {
-                            if (!p_var_3)
-                                res_1 = res_1 || var_1;
-                            else
-                                res_1 = res_1 || var_3;
-                        }
-                        else
-                            res_1 = var_1 || var_2;
-
-                        p_res_1 = true;
+                    if (p_var_3 && p_var_4){
+                        p_var_4 = false;
+                        var_3 = var_3 || var_4;
                     }
-                    else if (!p_res_2) {
-                        res_2 = var_1 || var_2;
-                        p_res_2 = true;
+                    else if (p_var_2 && p_var_3){
+                        p_var_3 = false;
+                        var_2 = var_2 || var_3;
                     }
-                    else
-                    {
-                        res_1 = res_1 || res_2;
-                        p_res_2 = false;
+                    else if (p_var_1 && p_var_2){
+                        p_var_2 = false;
+                        var_1 = var_1 || var_2;
                     }
 
-                    p_var_1 = false; p_var_2 = false;
+
                 }
+
+                cos = false;
             }
 
         }
@@ -318,10 +358,10 @@ vector <int> tab_truth(string& line)
         dic_v2.clear();
 
 
-        if (!p_res_1)
-            res_1 = var_1;
+//        if (!p_res_1)
+//            res_1 = var_1;
 
-        if (res_1)
+        if (var_1)
             table.push_back(1);
         else
             table.push_back(0);
@@ -336,6 +376,7 @@ void firstTask(){
     cout << "First expression: ";
     std::getline(cin, line1);
     line1 = del_space(line1);
+    cout << endl;
     cout << "Second expression: ";
     std::getline(cin, line2);
     line2 = del_space(line2);
@@ -411,8 +452,8 @@ string alu8(int f0, int f1, string a, string b, int inC){
 int d_latch = 0;
 
 void task_4(){
-    std::thread tA(dLatch, false);
-    std::thread tB(dLatch, true);
+    std::thread tA(dLatch1, false);
+    std::thread tB(dLatch1, true);
     tA.join();
     tB.join();
 }
@@ -443,6 +484,44 @@ void dLatch(bool choice){
         }
         else if (d_latch != -1)
             goto point;
+
+
+
+    }
+    else{
+        while (true){
+            cin >> d_latch;
+            if (d_latch == -1)
+                break;
+        }
+    }
+}
+
+
+void dLatch1(bool choice){
+    if (choice) {
+        int d_latch1 = 0;
+        int q, no_q, p;
+        q = 1;
+        no_q = 0;
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        std::chrono::duration<double, std::milli> fp_ms{};
+        std::chrono::steady_clock::time_point end;
+        point:
+        end = std::chrono::steady_clock::now();
+        fp_ms = end - begin;
+        if (fp_ms.count() >= 1000){
+            p = 1;
+            begin = end;
+
+        }
+        no_q = !((d_latch1 && p) || q);
+        q = !((!d_latch1 && p) || no_q);
+        no_q = !((d_latch1 && p) || q);
+        cout << q << " : " << no_q << endl;
+        p = 0;
+        usleep(250000);
+        goto point;
 
 
 
